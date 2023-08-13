664958e7892f7270654961b10b40907e1f3378bb
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-08-13 18:09:54 -0300

    feat: alterando rota /

--- api/app.py
+++ api/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
     return jsonify([{
-        'status': 'ok - release'
+        'status': 'OK'
     }])
 
 @app.route('/data', methods=['GET'])
