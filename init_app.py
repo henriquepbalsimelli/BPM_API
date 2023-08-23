2de8d4160c8fbd5f6741f4c9379a482adf11e04c
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-08-23 08:54:46 -0300

    debugando build

--- init_app.py
+++ init_app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from api import app as MyApp
 app = Flask(__name__)
 
+print('creating app')
 MyApp.create_app(app)
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000, debug=True)
+    print('running app')
+    app.run(host='0.0.0.0', port=5000, debug=True)
