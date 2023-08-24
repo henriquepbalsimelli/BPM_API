e0cd08c33731d5dbd5e57646f0d9515e01620eb9
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-08-24 20:06:39 -0300

    feat: ajustando requirements

--- api/ext/database.py
+++ api/ext/database.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, json
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
 
 db = SQLAlchemy()
 
@@ -14,5 +13,13 @@ __db_name = os.getenv('DB_NAME')
 __sqlalchemy_database_uri = 'mysql://' + __db_user + ':' + __db_pass + '@' + __db_host + ':' + __db_port + '/' + __db_name
 
 
-def init_app():
-    create_engine(__sqlalchemy_database_uri)
+def init_app(app: Flask):
+    __sqlalchemy_database_uri='mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user=__db_user, password=__db_pass, server=__db_host, database=__db_name)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = __sqlalchemy_database_uri
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100, 'pool_recycle': 90, 'json_serializer': json.dumps}
+
+
+    db.init_app(app)
