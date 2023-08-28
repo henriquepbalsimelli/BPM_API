6cd1b00549d2a8e849a0ceb47935075e67fe466c
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2023-08-28 20:51:48 -0300

    feat: tentando de novo

--- api/ext/database.py
+++ api/ext/database.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, json
 from flask_sqlalchemy import SQLAlchemy
-
+import logging
 db = SQLAlchemy()
 
 __db_host = os.getenv('DB_HOST')
@@ -10,16 +10,17 @@ __db_user = os.getenv('DB_USER')
 __db_pass = os.getenv('DB_PASS')
 __db_name = os.getenv('DB_NAME')
 
-__sqlalchemy_database_uri = 'mysql://' + __db_user + ':' + __db_pass + '@' + __db_host + ':' + __db_port + '/' + __db_name
-
 
 def init_app(app: Flask):
-    __sqlalchemy_database_uri='mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user=__db_user, password=__db_pass, server=__db_host, database=__db_name)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = __sqlalchemy_database_uri
-    app.config['SQLALCHEMY_ECHO'] = False
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100, 'pool_recycle': 90, 'json_serializer': json.dumps}
+    try:
+        __sqlalchemy_database_uri='mysql+pymysql://{user}:{password}@{server}:{port}/{database}'.format(user=__db_user, password=__db_pass, server=__db_host, port=__db_port, database='BPM')
+        
+        app.config['SQLALCHEMY_DATABASE_URI'] = __sqlalchemy_database_uri
+        app.config['SQLALCHEMY_ECHO'] = False
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100, 'pool_recycle': 90, 'json_serializer': json.dumps}
 
 
-    db.init_app(app)
+        db.init_app(app)
+    except Exception as error:
+        logging.exception(error)
--- api/init_app.py
+++ api/init_app.py
@@ -4,9 +4,8 @@ from api import blueprints
 
 def create_app():
     app = Flask(__name__)
-    #database.init_app(app)
+    database.init_app(app)
     blueprints.init_app(app)
-
 
     @app.route('/', methods=['GET', 'POST'])
     def welcome():
