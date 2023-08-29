2712ba6a83f56b22aae8e1de880be81617c56dad
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2023-08-28 22:00:27 -0300

    tentando com createe-negine

--- api/ext/database.py
+++ api/ext/database.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, json
 from flask_sqlalchemy import SQLAlchemy
 import logging
+from sqlalchemy import create_engine
 db = SQLAlchemy()
 
 __db_host = os.getenv('DB_HOST')
@@ -14,13 +15,14 @@ __db_name = os.getenv('DB_NAME')
 def init_app(app: Flask):
     try:
         __sqlalchemy_database_uri='mysql://{user}:{password}@{server}:{port}/{database}'.format(user=__db_user, password=__db_pass, server=__db_host, port=__db_port, database='BPM')
-        
-        app.config['SQLALCHEMY_DATABASE_URI'] = __sqlalchemy_database_uri
-        app.config['SQLALCHEMY_BINDS'] = {
-            'BPM': __sqlalchemy_database_uri
-        }
-        app.config['SQLALCHEMY_ECHO'] = False
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        engine = create_engine(__sqlalchemy_database_uri)
+
+        # app.config['SQLALCHEMY_DATABASE_URI'] = __sqlalchemy_database_uri
+        # app.config['SQLALCHEMY_BINDS'] = {
+        #     'BPM': __sqlalchemy_database_uri
+        # }
+        # app.config['SQLALCHEMY_ECHO'] = False
+        # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
         db.init_app(app)
     except Exception as error:
