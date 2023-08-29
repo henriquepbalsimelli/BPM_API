06c84ead06584a65ca301ddef8f3f51d97a39586
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2023-08-28 21:10:31 -0300

    a

--- api/ext/database.py
+++ api/ext/database.py
@@ -12,12 +12,16 @@ __db_name = os.getenv('DB_NAME')
 
 
 def init_app(app: Flask):
-    __sqlalchemy_database_uri='mysql+pymysql://{user}:{password}@{server}:{port}/{database}'.format(user=__db_user, password=__db_pass, server=__db_host, port=__db_port, database='BPM')
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = __sqlalchemy_database_uri
-    app.config['SQLALCHEMY_BINDS'] = None
-    app.config['SQLALCHEMY_ECHO'] = False
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100, 'pool_recycle': 90, 'json_serializer': json.dumps}
+    try:
+        __sqlalchemy_database_uri='mysql+pymysql://{user}:{password}@{server}:{port}/{database}'.format(user=__db_user, password=__db_pass, server=__db_host, port=__db_port, database='BPM')
+        
+        app.config['SQLALCHEMY_DATABASE_URI'] = __sqlalchemy_database_uri
+        app.config['SQLALCHEMY_BINDS'] = {}
+        app.config['SQLALCHEMY_ECHO'] = False
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100, 'pool_recycle': 90, 'json_serializer': json.dumps}
 
-    db.init_app(app)
+
+        db.init_app(app)
+    except Exception as error:
+        logging.exception(error)
